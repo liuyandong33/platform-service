97002df9fedd5f76439f078d15e1950866749318
liuyandong <554030404@qq.com>
2017-07-25 17:45:54 +0800

    初始化项目！！！

---
+++ src/main/java/build/dream/platform/Application.java
@@ -0,0 +1,12 @@
+package build.dream.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.run(args);
+    }
+}
