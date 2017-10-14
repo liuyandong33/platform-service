2421b2b97c7f986d81d6199ee1875a829442d07a
liuyandong <554030404@qq.com>
2017-10-14 22:22:05 +0800

    初始化库！！！

--- src/main/java/build/dream/platform/controllers/TenantController.java
+++ src/main/java/build/dream/platform/controllers/TenantController.java
@@ -1,8 +1,14 @@
 package build.dream.platform.controllers;
 
 import build.dream.common.api.ApiRest;
+import build.dream.common.controllers.BasicController;
 import build.dream.common.utils.ApplicationHandler;
 import build.dream.common.utils.GsonUtils;
+import build.dream.common.utils.LogUtils;
+import build.dream.platform.services.TenantService;
+import org.apache.commons.lang.Validate;
+import org.apache.commons.lang.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,15 +17,24 @@ import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/tenant")
-public class TenantController {
+public class TenantController extends BasicController {
+    @Autowired
+    private TenantService tenantService;
 
-    @RequestMapping(value = "/index")
+    @RequestMapping(value = "/findTenantInfoById")
     @ResponseBody
-    public String index() {
-        ApiRest apiRest = new ApiRest();
-        apiRest.setData(ApplicationHandler.getRequestParameters());
-        ApiRest apiRest1 = GsonUtils.fromJson(GsonUtils.toJson(apiRest), ApiRest.class);
-        Map<String, String> data = (Map<String, String>) apiRest1.getData();
+    public String findTenantInfoById() {
+        ApiRest apiRest = null;
+        Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
+        try {
+            String tenantId = requestParameters.get("tenantId");
+            Validate.notNull(tenantId, ApplicationHandler.obtainParameterErrorMessage("tenantId"));
+
+            apiRest = tenantService.findTenantInfoById(NumberUtils.createBigInteger(tenantId));
+        } catch (Exception e) {
+            LogUtils.error("查询商户信息失败", controllerSimpleName, "findTenantInfoById", e, requestParameters);
+            apiRest = new ApiRest(e);
+        }
         return GsonUtils.toJson(apiRest);
     }
 }
---
+++ src/main/java/build/dream/platform/services/TenantService.java
@@ -0,0 +1,33 @@
+package build.dream.platform.services;
+
+import build.dream.common.api.ApiRest;
+import build.dream.common.saas.domains.Tenant;
+import build.dream.common.utils.SearchModel;
+import build.dream.platform.constants.Constants;
+import build.dream.platform.mappers.TenantMapper;
+import org.apache.commons.lang.Validate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigInteger;
+
+@Service
+public class TenantService {
+    @Autowired
+    private TenantMapper tenantMapper;
+
+    @Transactional(readOnly = true)
+    public ApiRest findTenantInfoById(BigInteger tenantId) {
+        SearchModel searchModel = new SearchModel(true);
+        searchModel.addSearchCondition("id", Constants.SQL_OPERATION_SYMBOL_EQUALS, tenantId);
+        Tenant tenant = tenantMapper.find(searchModel);
+
+        ApiRest apiRest = new ApiRest();
+        apiRest.setClassName(Tenant.class.getName());
+        apiRest.setData(tenant);
+        apiRest.setMessage("查询商户信息成功！");
+        apiRest.setSuccessful(true);
+        return apiRest;
+    }
+}
