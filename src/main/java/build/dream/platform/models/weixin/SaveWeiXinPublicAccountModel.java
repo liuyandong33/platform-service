434dc75f25b266719f8a7a06488865149a386c33
liuyandong <554030404@qq.com>
2017-12-31 21:19:34 +0800

    商品接口开发！！

---
+++ src/main/java/build/dream/platform/models/weixin/SaveWeiXinPublicAccountModel.java
@@ -0,0 +1,85 @@
+package build.dream.platform.models.weixin;
+
+import build.dream.common.models.BasicModel;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigInteger;
+
+public class SaveWeiXinPublicAccountModel extends BasicModel {
+    @NotNull
+    private BigInteger userId;
+
+    @NotNull
+    private BigInteger tenantId;
+
+    @NotNull
+    private BigInteger branchId;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String appId;
+
+    @NotNull
+    private String appSecret;
+
+    @NotNull
+    private String originalId;
+
+    public BigInteger getUserId() {
+        return userId;
+    }
+
+    public void setUserId(BigInteger userId) {
+        this.userId = userId;
+    }
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public BigInteger getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(BigInteger branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+    }
+
+    public String getOriginalId() {
+        return originalId;
+    }
+
+    public void setOriginalId(String originalId) {
+        this.originalId = originalId;
+    }
+}
