3cc0224c80265f1735ae003d9c97dcfc3568d026
liuyandong <554030404@qq.com>
2018-02-02 10:07:01 +0800

    初始化项目！！！

--- src/main/java/build/dream/platform/controllers/ActivityController.java
+++ src/main/java/build/dream/platform/controllers/ActivityController.java
@@ -2,10 +2,7 @@ package build.dream.platform.controllers;
 
 import build.dream.common.api.ApiRest;
 import build.dream.common.controllers.BasicController;
-import build.dream.common.utils.ApplicationHandler;
-import build.dream.common.utils.GsonUtils;
-import build.dream.common.utils.LogUtils;
-import build.dream.common.utils.QRCodeUtils;
+import build.dream.common.utils.*;
 import build.dream.platform.models.activity.SaveSpecialGoodsActivityModel;
 import build.dream.platform.services.ActivityService;
 import com.google.zxing.WriterException;
@@ -14,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
@@ -41,13 +39,29 @@ public class ActivityController extends BasicController {
         return GsonUtils.toJson(apiRest);
     }
 
-    @RequestMapping(value = "test")
+    @RequestMapping(value = "generateQRCode")
     @ResponseBody
-    public void test() throws IOException, WriterException {
+    public void generateQRCode() throws IOException, WriterException {
         Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
         int width = Integer.valueOf(requestParameters.get("width"));
         int height = Integer.valueOf(requestParameters.get("width"));
         String text = requestParameters.get("text");
-        QRCodeUtils.generateQRCode(width, height, text, ApplicationHandler.getHttpServletResponse().getOutputStream());
+        HttpServletResponse httpServletResponse = ApplicationHandler.getHttpServletResponse();
+        httpServletResponse.setContentType(MimeMappingUtils.obtainMimeTypeByExtension(QRCodeUtils.FORMAT));
+        QRCodeUtils.generateQRCode(width, height, text, httpServletResponse.getOutputStream());
+    }
+
+    @RequestMapping(value = "downloadQRCode")
+    @ResponseBody
+    public void downloadQRCode() throws IOException, WriterException {
+        Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
+        int width = Integer.valueOf(requestParameters.get("width"));
+        int height = Integer.valueOf(requestParameters.get("width"));
+        String text = requestParameters.get("text");
+        String fileName = requestParameters.get("fileName");
+        HttpServletResponse httpServletResponse = ApplicationHandler.getHttpServletResponse();
+        httpServletResponse.setContentType(MimeMappingUtils.obtainMimeTypeByExtension(QRCodeUtils.FORMAT));
+        httpServletResponse.setHeader("Content-disposition", "attachment;filename=" + fileName + ".png");
+        QRCodeUtils.generateQRCode(width, height, text, httpServletResponse.getOutputStream());
     }
 }
