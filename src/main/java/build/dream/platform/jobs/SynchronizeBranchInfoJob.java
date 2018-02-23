6c61e1fbce6fe0210833a3e213c56651e4601028
liuyandong <554030404@qq.com>
2018-02-23 14:52:11 +0800

    初始化项目！！！

--- src/main/java/build/dream/platform/jobs/SynchronizeBranchInfoJob.java
+++ src/main/java/build/dream/platform/jobs/SynchronizeBranchInfoJob.java
@@ -1,11 +1,10 @@
 package build.dream.platform.jobs;
 
+import build.dream.common.utils.LogUtils;
 import build.dream.platform.services.BranchService;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.io.IOException;
 
 public class SynchronizeBranchInfoJob implements Job {
     private static final String SYNCHRONIZE_BRANCH_INFO_JOB_SIMPLE_NAME = "SynchronizeBranchInfoJob";
@@ -17,7 +16,7 @@ public class SynchronizeBranchInfoJob implements Job {
         try {
             branchService.synchronizeBranchInfo();
         } catch (Exception e) {
-            e.printStackTrace();
+            LogUtils.error("同步门店信息失败", SYNCHRONIZE_BRANCH_INFO_JOB_SIMPLE_NAME, "execute", e);
         }
     }
 }
