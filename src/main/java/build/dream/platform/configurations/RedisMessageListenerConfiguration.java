581ef3db50e0a6171a8ec0355bb5565018219c69
liuyandong <554030404@qq.com>
2018-04-14 10:06:58 +0800

    初始化项目！！！

--- src/main/java/build/dream/platform/configurations/RedisMessageListenerConfiguration.java
+++ src/main/java/build/dream/platform/configurations/RedisMessageListenerConfiguration.java
@@ -1,13 +1,9 @@
 package build.dream.platform.configurations;
 
-import build.dream.common.utils.ConfigurationUtils;
-import build.dream.platform.constants.Constants;
-import build.dream.platform.listeners.ElemeCallbackMessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 import java.io.IOException;
@@ -15,16 +11,12 @@ import java.io.IOException;
 @Configuration
 public class RedisMessageListenerConfiguration {
     @Autowired
-    private JedisConnectionFactory jedisConnectionFactory;
-    @Autowired
-    private ElemeCallbackMessageListener elemeCallbackMessageListener;
+    private RedisConnectionFactory redisConnectionFactory;
 
     @Bean(destroyMethod = "destroy")
     public RedisMessageListenerContainer redisMessageListenerContainer() throws IOException {
         RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
-        redisMessageListenerContainer.setConnectionFactory(jedisConnectionFactory);
-        String elemeCallbackMessageChannelTopic = ConfigurationUtils.getConfiguration(Constants.ELEME_CALLBACK_MESSAGE_CHANNEL_TOPIC) + "_" + ConfigurationUtils.getConfiguration(Constants.PARTITION_CODE);
-        redisMessageListenerContainer.addMessageListener(elemeCallbackMessageListener, new ChannelTopic(elemeCallbackMessageChannelTopic));
+        redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory);
         return redisMessageListenerContainer;
     }
 }
