29fad66a474410541a54ff091ba1720a60f53c3a
liuyandong <554030404@qq.com>
2018-04-24 22:44:35 +0800

    商品接口开发！！

--- src/main/scala/build/dream/platform/controllers/DemoController.scala
+++ src/main/scala/build/dream/platform/controllers/DemoController.scala
@@ -1,42 +1,84 @@
 package build.dream.platform.controllers
 
-import java.lang.reflect.Field
 import java.util.Map
 
-import build.dream.common.api.ApiRest
-import build.dream.common.controllers.BasicController
-import build.dream.common.erp.catering.domains.Branch
-import build.dream.common.utils.{ApplicationHandler, GsonUtils, LogUtils}
+import build.dream.common.utils.{ApplicationHandler, MethodCaller}
+import build.dream.platform.models.user.{BatchDeleteUserModel, BatchGetUsersModel, ObtainAllPrivilegesModel, ObtainUserInfoModel}
+import build.dream.platform.services.UserService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, ResponseBody}
 
 @Controller
 @RequestMapping(value = Array("/demo"))
-class DemoController extends BasicController {
-    @RequestMapping(value = Array("/index"), method = Array(RequestMethod.GET))
+class DemoController {
+    @Autowired
+    private val userService: UserService = null
+
+    /**
+      * 获取用户信息
+      *
+      * @return
+      */
+    @RequestMapping(value = Array("/obtainUserInfo"), method = Array(RequestMethod.GET))
     @ResponseBody
-    def index(): String = {
-        var apiRest: ApiRest = null
+    def index: String = {
         val requestParameters: Map[String, String] = ApplicationHandler.getRequestParameters
-        try {
-            val branchClass: Class[Branch] = classOf[Branch]
-            val fields: Array[Field] = branchClass.getDeclaredFields
-
-            for (field: Field <- fields) {
-                println(field.getName)
-            }
-
-            apiRest = new ApiRest()
-            apiRest setMessage ("测试成功")
-            apiRest setSuccessful (true)
-        } catch {
-            case (e: Exception) => {
-                LogUtils.error("测试失败", controllerSimpleName, "index", e, requestParameters)
-                apiRest = new ApiRest()
-                apiRest.setMessage(e.getMessage)
-                apiRest.setSuccessful(true)
-            }
+        val methodCaller: MethodCaller = () => {
+            val obtainUserInfoModel = ApplicationHandler.instantiateObject(classOf[ObtainUserInfoModel], requestParameters)
+            userService.obtainUserInfo(obtainUserInfoModel)
         }
-        GsonUtils.toJson(apiRest)
+        ApplicationHandler.callMethod(methodCaller, "获取用户信息失败", requestParameters)
+    }
+
+    /**
+      * 批量获取用户信息
+      *
+      * @return
+      */
+    @RequestMapping(value = Array("/batchGetUsers"), method = Array(RequestMethod.GET))
+    @ResponseBody
+    def batchGetUsers: String = {
+        val requestParameters: Map[String, String] = ApplicationHandler.getRequestParameters
+        val methodCaller: MethodCaller = () => {
+            val batchGetUsersModel: BatchGetUsersModel = ApplicationHandler.instantiateObject(classOf[BatchGetUsersModel], requestParameters)
+            batchGetUsersModel.validateAndThrow()
+            userService.batchObtainUser(batchGetUsersModel)
+        }
+        ApplicationHandler.callMethod(methodCaller, "查询用户失败", requestParameters)
+    }
+
+    /**
+      * 获取用户所有的权限
+      *
+      * @return
+      */
+    @RequestMapping(value = Array("/obtainAllPrivileges"), method = Array(RequestMethod.GET))
+    @ResponseBody
+    def obtainAllPrivileges: String = {
+        val requestParameters: Map[String, String] = ApplicationHandler.getRequestParameters
+        val methodCaller: MethodCaller = () => {
+            val obtainAllPrivilegesModel: ObtainAllPrivilegesModel = ApplicationHandler.instantiateObject(classOf[ObtainAllPrivilegesModel], requestParameters)
+            obtainAllPrivilegesModel.validateAndThrow()
+            userService.obtainAllPrivileges(obtainAllPrivilegesModel)
+        }
+        ApplicationHandler.callMethod(methodCaller, "获取用户权限失败", requestParameters)
+    }
+
+    /**
+      * 批量删除用户
+      *
+      * @return
+      */
+    @RequestMapping(value = Array("/batchDeleteUser"), method = Array(RequestMethod.POST))
+    @ResponseBody
+    def batchDeleteUser: String = {
+        val requestParameters: Map[String, String] = ApplicationHandler.getRequestParameters
+        val methodCaller: MethodCaller = () => {
+            val batchDeleteUserModel: BatchDeleteUserModel = ApplicationHandler.instantiateObject(classOf[BatchDeleteUserModel], requestParameters)
+            batchDeleteUserModel.validateAndThrow()
+            userService.batchDeleteUser(batchDeleteUserModel)
+        }
+        ApplicationHandler.callMethod(methodCaller, "批量删除用户失败", requestParameters)
     }
 }
