6ebdd3cba8de4502b32b75be7e6b9cd0ccc3bc5c
liuyandong <554030404@qq.com>
2018-05-18 17:12:22 +0800

    初始化项目！！！

--- src/main/java/build/dream/platform/mappers/OrderDetailMapper.java
+++ src/main/java/build/dream/platform/mappers/OrderDetailMapper.java
@@ -2,13 +2,10 @@ package build.dream.platform.mappers;
 
 import build.dream.common.saas.domains.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
-import java.math.BigInteger;
 import java.util.List;
 
 @Mapper
 public interface OrderDetailMapper {
     long insertAll(List<OrderDetail> orderDetails);
-    long deleteAllByOrderIds(@Param("orderIds") List<BigInteger> orderIds, @Param("lastUpdateRemark") String lastUpdateRemark);
 }
