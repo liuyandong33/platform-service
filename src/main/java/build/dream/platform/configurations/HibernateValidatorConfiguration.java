96ca98b236fef763071fad6f1d2a676fef84bc45
liuyandong <554030404@qq.com>
2018-05-31 18:01:46 +0800

    商品接口开发！！

--- src/main/java/build/dream/platform/configurations/HibernateValidatorConfiguration.java
+++ src/main/java/build/dream/platform/configurations/HibernateValidatorConfiguration.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.validation.Validator;
 
 @Configuration
 public class HibernateValidatorConfiguration {
