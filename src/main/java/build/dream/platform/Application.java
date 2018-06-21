862671f711338ffb757120df1cde4a596b9be783
liuyandong <554030404@qq.com>
2018-06-21 23:18:06 +0800

    商品接口开发！！

--- src/main/java/build/dream/platform/Application.java
+++ src/main/java/build/dream/platform/Application.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @ServletComponentScan
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
