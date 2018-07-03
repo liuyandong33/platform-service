8b4ab2909f740acb26cf11e1fb187c6ac62f4920
liuyandong <554030404@qq.com>
2018-07-03 11:09:39 +0800

    初始化项目！！！

--- src/main/java/build/dream/platform/listeners/PlatformServiceServletContextListener.java
+++ src/main/java/build/dream/platform/listeners/PlatformServiceServletContextListener.java
@@ -1,35 +1,26 @@
 package build.dream.platform.listeners;
 
 import build.dream.common.listeners.BasicServletContextListener;
-import build.dream.common.saas.domains.SystemUser;
 import build.dream.common.saas.domains.TenantSecretKey;
-import build.dream.common.utils.*;
+import build.dream.common.utils.CacheUtils;
+import build.dream.common.utils.ConfigurationUtils;
+import build.dream.common.utils.LogUtils;
+import build.dream.common.utils.SearchModel;
 import build.dream.platform.constants.Constants;
 import build.dream.platform.jobs.JobScheduler;
 import build.dream.platform.mappers.CommonMapper;
-import build.dream.platform.services.ConfigurationService;
-import build.dream.platform.services.SystemPartitionService;
-import build.dream.platform.services.SystemUserService;
 import build.dream.platform.services.TenantSecretKeyService;
 import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
-import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @WebListener
 public class PlatformServiceServletContextListener extends BasicServletContextListener {
-    private static final String PLATFORM_SERVICE_SERVLET_CONTEXT_LISTENER_SIMPLE_NAME = "PlatformServiceServletContextListener";
-    @Autowired
-    private SystemPartitionService systemPartitionService;
-    @Autowired
-    private ConfigurationService configurationService;
-    @Autowired
-    private SystemUserService systemUserService;
     @Autowired
     private TenantSecretKeyService tenantSecretKeyService;
     @Autowired
@@ -40,11 +31,6 @@ public class PlatformServiceServletContextListener extends BasicServletContextLi
         super.contextInitialized(servletContextEvent);
         super.previousInjectionBean(servletContextEvent.getServletContext(), CommonMapper.class);
         try {
-            SearchModel systemUserSearchModel = new SearchModel(true);
-            systemUserSearchModel.addSearchCondition("user_type", Constants.SQL_OPERATION_SYMBOL_EQUAL, BigInteger.valueOf(3));
-            List<SystemUser> systemUsers = systemUserService.findAll(systemUserSearchModel);
-            CommonUtils.loadServiceSystemUsers(systemUsers);
-
             SearchModel tenantSecretKeySearchModel = new SearchModel(true);
             List<TenantSecretKey> tenantSecretKeys = tenantSecretKeyService.findAll(tenantSecretKeySearchModel);
             Map<String, String> tenantPublicKeys = new HashMap<String, String>();
@@ -61,7 +47,7 @@ public class PlatformServiceServletContextListener extends BasicServletContextLi
             // 启动所有定时任务
             jobScheduler.scheduler();
         } catch (Exception e) {
-            LogUtils.error("初始化数据失败", PLATFORM_SERVICE_SERVLET_CONTEXT_LISTENER_SIMPLE_NAME, "contextInitialized", e);
+            LogUtils.error("初始化数据失败", this.getClass().getName(), "contextInitialized", e);
         }
     }
 
