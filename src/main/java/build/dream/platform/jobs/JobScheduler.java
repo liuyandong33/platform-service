d052746b2a013bd2d0a186609675e98445b30d77
liuyandong <554030404@qq.com>
2018-07-07 16:01:28 +0800

    商品接口开发！！

--- src/main/java/build/dream/platform/jobs/JobScheduler.java
+++ src/main/java/build/dream/platform/jobs/JobScheduler.java
@@ -7,34 +7,35 @@ import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Component;
-import sun.security.krb5.Config;
-
-import java.io.IOException;
 
 @Component
 public class JobScheduler {
     @Autowired
     SchedulerFactoryBean schedulerFactoryBean;
 
-    public void scheduler() throws IOException, SchedulerException {
-        Scheduler scheduler = schedulerFactoryBean.getScheduler();
+    public void scheduler() {
+        try {
+            Scheduler scheduler = schedulerFactoryBean.getScheduler();
 
-        // 启动同步门店信息定时任务
-        String synchronizeBranchInfoJobCronExpression = ConfigurationUtils.getConfiguration(Constants.SYNCHRONIZE_BRANCH_INFO_JOB_CRON_EXPRESSION);
-        if (StringUtils.isNotBlank(synchronizeBranchInfoJobCronExpression)) {
-            JobDetail synchronizeBranchInfoJobDetail = JobBuilder.newJob(SynchronizeBranchInfoJob.class).withIdentity("synchronizeBranchInfoJob", "platformJobGroup").build();
-            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(synchronizeBranchInfoJobCronExpression);
-            Trigger synchronizeBranchInfoJobCronTrigger = TriggerBuilder.newTrigger().withIdentity("synchronizeBranchInfoJobTrigger", "platformJobGroup").withSchedule(cronScheduleBuilder).build();
-            scheduler.scheduleJob(synchronizeBranchInfoJobDetail, synchronizeBranchInfoJobCronTrigger);
-        }
+            // 启动同步门店信息定时任务
+            String synchronizeBranchInfoJobCronExpression = ConfigurationUtils.getConfiguration(Constants.SYNCHRONIZE_BRANCH_INFO_JOB_CRON_EXPRESSION);
+            if (StringUtils.isNotBlank(synchronizeBranchInfoJobCronExpression)) {
+                JobDetail synchronizeBranchInfoJobDetail = JobBuilder.newJob(SynchronizeBranchInfoJob.class).withIdentity("synchronizeBranchInfoJob", "platformJobGroup").build();
+                CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(synchronizeBranchInfoJobCronExpression);
+                Trigger synchronizeBranchInfoJobCronTrigger = TriggerBuilder.newTrigger().withIdentity("synchronizeBranchInfoJobTrigger", "platformJobGroup").withSchedule(cronScheduleBuilder).build();
+                scheduler.scheduleJob(synchronizeBranchInfoJobDetail, synchronizeBranchInfoJobCronTrigger);
+            }
 
-        // 启用禁用门店产品定时任务
-        String disableBranchGoodsJobCronExpression = ConfigurationUtils.getConfiguration(Constants.DISABLE_BRANCH_GOODS_JOB_CRON_EXPRESSION);
-        if (StringUtils.isNotBlank(disableBranchGoodsJobCronExpression)) {
-            JobDetail disableBranchGoodsJobDetail = JobBuilder.newJob(DisableBranchGoodsJob.class).withIdentity("disableBranchGoodsJob", "platformJobGroup").build();
-            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(disableBranchGoodsJobCronExpression);
-            Trigger disableBranchGoodsJobCronTrigger = TriggerBuilder.newTrigger().withIdentity("disableBranchGoodsJobTrigger", "platformJobGroup").withSchedule(cronScheduleBuilder).build();
-            scheduler.scheduleJob(disableBranchGoodsJobDetail, disableBranchGoodsJobCronTrigger);
+            // 启用禁用门店产品定时任务
+            String disableBranchGoodsJobCronExpression = ConfigurationUtils.getConfiguration(Constants.DISABLE_BRANCH_GOODS_JOB_CRON_EXPRESSION);
+            if (StringUtils.isNotBlank(disableBranchGoodsJobCronExpression)) {
+                JobDetail disableBranchGoodsJobDetail = JobBuilder.newJob(DisableBranchGoodsJob.class).withIdentity("disableBranchGoodsJob", "platformJobGroup").build();
+                CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(disableBranchGoodsJobCronExpression);
+                Trigger disableBranchGoodsJobCronTrigger = TriggerBuilder.newTrigger().withIdentity("disableBranchGoodsJobTrigger", "platformJobGroup").withSchedule(cronScheduleBuilder).build();
+                scheduler.scheduleJob(disableBranchGoodsJobDetail, disableBranchGoodsJobCronTrigger);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/build/dream/platform/listeners/PlatformServiceServletContextListener.java
+++ src/main/java/build/dream/platform/listeners/PlatformServiceServletContextListener.java
@@ -7,7 +7,6 @@ import build.dream.common.saas.domains.TenantSecretKey;
 import build.dream.common.utils.CacheUtils;
 import build.dream.common.utils.ConfigurationUtils;
 import build.dream.common.utils.GsonUtils;
-import build.dream.common.utils.LogUtils;
 import build.dream.platform.constants.Constants;
 import build.dream.platform.jobs.JobScheduler;
 import build.dream.platform.mappers.CommonMapper;
@@ -38,47 +37,43 @@ public class PlatformServiceServletContextListener extends BasicServletContextLi
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         super.contextInitialized(servletContextEvent);
         super.previousInjectionBean(servletContextEvent.getServletContext(), CommonMapper.class);
-        try {
-            List<AlipayAccount> alipayAccounts = alipayService.findAllAlipayAccounts();
-            Map<String, String> alipayAccountMap = new HashMap<String, String>();
-            for (AlipayAccount alipayAccount : alipayAccounts) {
-                alipayAccountMap.put(alipayAccount.getTenantId() + "_" + alipayAccount.getBranchId(), GsonUtils.toJson(alipayAccount));
-            }
-
-            CacheUtils.delete(Constants.KEY_ALIPAY_ACCOUNTS);
-            if (MapUtils.isNotEmpty(alipayAccountMap)) {
-                CacheUtils.hmset(Constants.KEY_ALIPAY_ACCOUNTS, alipayAccountMap);
-            }
-
-            List<TenantSecretKey> tenantSecretKeys = tenantSecretKeyService.findAll();
-            Map<String, String> tenantPublicKeys = new HashMap<String, String>();
-            for (TenantSecretKey tenantSecretKey : tenantSecretKeys) {
-                tenantPublicKeys.put(tenantSecretKey.getTenantId().toString(), tenantSecretKey.getPublicKey());
-            }
-            CacheUtils.delete(Constants.KEY_TENANT_PUBLIC_KEYS);
-            if (MapUtils.isNotEmpty(tenantPublicKeys)) {
-                CacheUtils.hmset(Constants.KEY_TENANT_PUBLIC_KEYS, tenantPublicKeys);
-            }
-
-            CacheUtils.set(Constants.KEY_PLATFORM_PRIVATE_KEY, ConfigurationUtils.getConfiguration(Constants.PLATFORM_PRIVATE_KEY));
-
-            CacheUtils.delete(Constants.KEY_TENANT_INFOS);
-            List<Tenant> tenants = tenantService.obtainAllTenantInfos();
-            Map<String, String> tenantInfos = new HashMap<String, String>();
-            for (Tenant tenant : tenants) {
-                String tenantInfo = GsonUtils.toJson(tenant);
-                tenantInfos.put(tenant.getId().toString(), tenantInfo);
-                tenantInfos.put(tenant.getCode(), tenantInfo);
-            }
-            if (MapUtils.isNotEmpty(tenantInfos)) {
-                CacheUtils.hmset(Constants.KEY_TENANT_INFOS, tenantInfos);
-            }
-
-            // 启动所有定时任务
-            jobScheduler.scheduler();
-        } catch (Exception e) {
-            LogUtils.error("初始化数据失败", this.getClass().getName(), "contextInitialized", e);
+        List<AlipayAccount> alipayAccounts = alipayService.findAllAlipayAccounts();
+        Map<String, String> alipayAccountMap = new HashMap<String, String>();
+        for (AlipayAccount alipayAccount : alipayAccounts) {
+            alipayAccountMap.put(alipayAccount.getTenantId() + "_" + alipayAccount.getBranchId(), GsonUtils.toJson(alipayAccount));
         }
+
+        CacheUtils.delete(Constants.KEY_ALIPAY_ACCOUNTS);
+        if (MapUtils.isNotEmpty(alipayAccountMap)) {
+            CacheUtils.hmset(Constants.KEY_ALIPAY_ACCOUNTS, alipayAccountMap);
+        }
+
+        List<TenantSecretKey> tenantSecretKeys = tenantSecretKeyService.findAll();
+        Map<String, String> tenantPublicKeys = new HashMap<String, String>();
+        for (TenantSecretKey tenantSecretKey : tenantSecretKeys) {
+            tenantPublicKeys.put(tenantSecretKey.getTenantId().toString(), tenantSecretKey.getPublicKey());
+        }
+        CacheUtils.delete(Constants.KEY_TENANT_PUBLIC_KEYS);
+        if (MapUtils.isNotEmpty(tenantPublicKeys)) {
+            CacheUtils.hmset(Constants.KEY_TENANT_PUBLIC_KEYS, tenantPublicKeys);
+        }
+
+        CacheUtils.set(Constants.KEY_PLATFORM_PRIVATE_KEY, ConfigurationUtils.getConfigurationSafe(Constants.PLATFORM_PRIVATE_KEY));
+
+        CacheUtils.delete(Constants.KEY_TENANT_INFOS);
+        List<Tenant> tenants = tenantService.obtainAllTenantInfos();
+        Map<String, String> tenantInfos = new HashMap<String, String>();
+        for (Tenant tenant : tenants) {
+            String tenantInfo = GsonUtils.toJson(tenant);
+            tenantInfos.put(tenant.getId().toString(), tenantInfo);
+            tenantInfos.put(tenant.getCode(), tenantInfo);
+        }
+        if (MapUtils.isNotEmpty(tenantInfos)) {
+            CacheUtils.hmset(Constants.KEY_TENANT_INFOS, tenantInfos);
+        }
+
+        // 启动所有定时任务
+        jobScheduler.scheduler();
     }
 
     @Override
