02451abcb664c1ca39d287e740423e9cf173cb61
liuyandong <554030404@qq.com>
2018-07-15 01:12:23 +0800

    商品接口开发！！

--- src/main/java/build/dream/platform/aspects/DataSourceRoutingAspect.java
+++ src/main/java/build/dream/platform/aspects/DataSourceRoutingAspect.java
@@ -1,6 +1,5 @@
 package build.dream.platform.aspects;
 
-import build.dream.common.annotations.DataSourceType;
 import build.dream.platform.configurations.DataSourceContextHolder;
 import build.dream.platform.constants.Constants;
 import org.aspectj.lang.JoinPoint;
@@ -9,9 +8,9 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.reflect.Method;
-import java.util.UUID;
 
 @Aspect
 @Component
@@ -21,21 +20,15 @@ public class DataSourceRoutingAspect {
     public void setDataSource(JoinPoint joinPoint) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Method method = methodSignature.getMethod();
-        DataSourceType dataSourceType = method.getAnnotation(DataSourceType.class);
-        if (dataSourceType != null) {
-            joinPoint.getTarget().getClass().getAnnotation(DataSourceType.class);
-        }
 
-        if (dataSourceType != null) {
-            int type = dataSourceType.type();
-            if (type == Constants.DATA_SOURCE_TYPE_READ) {
-                DataSourceContextHolder.setDataSourceType(Constants.SECONDARY_DATA_SOURCE);
-            } else if (type == Constants.DATA_SOURCE_TYPE_WRITE) {
-                DataSourceContextHolder.setDataSourceType(Constants.PRIMARY_DATA_SOURCE);
-            }
+        Transactional transactional = method.getAnnotation(Transactional.class);
+        if (transactional == null) {
+            transactional = joinPoint.getTarget().getClass().getAnnotation(Transactional.class);
+        }
+        if (transactional != null && transactional.readOnly()) {
+            DataSourceContextHolder.setDataSourceType(Constants.SECONDARY_DATA_SOURCE);
         } else {
             DataSourceContextHolder.setDataSourceType(Constants.PRIMARY_DATA_SOURCE);
         }
-        System.out.println(UUID.randomUUID().toString());
     }
 }
