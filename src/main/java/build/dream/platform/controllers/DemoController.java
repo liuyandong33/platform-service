4c18b255a40fe3b4b5246ccd521b388780d35bb1
liuyandong <554030404@qq.com>
2018-09-13 10:00:42 +0800

    微信相关接口开发！！

--- src/main/java/build/dream/platform/controllers/DemoController.java
+++ src/main/java/build/dream/platform/controllers/DemoController.java
@@ -1,6 +1,7 @@
 package build.dream.platform.controllers;
 
 import build.dream.common.models.newland.BarcodePayModel;
+import build.dream.common.models.newland.BarcodePosPayModel;
 import build.dream.common.utils.ApplicationHandler;
 import build.dream.common.utils.GsonUtils;
 import build.dream.common.utils.NewLandUtils;
@@ -13,13 +14,26 @@ import java.util.Map;
 @Controller
 @RequestMapping(value = "/demo")
 public class DemoController {
-    @RequestMapping(value = "/demo")
+    @RequestMapping(value = "/barcodePay")
     @ResponseBody
-    public String demo() {
+    public String barcodePay() {
         Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
         try {
             BarcodePayModel barcodePayModel = ApplicationHandler.instantiateObject(BarcodePayModel.class, requestParameters);
             Map<String, String> result = NewLandUtils.barcodePay("1", "1", barcodePayModel);
+            return GsonUtils.toJson(result);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping(value = "/barcodePosPay")
+    @ResponseBody
+    public String barcodePosPay() {
+        Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
+        try {
+            BarcodePosPayModel barcodePosPayModel = ApplicationHandler.instantiateObject(BarcodePosPayModel.class, requestParameters);
+            Map<String, String> result = NewLandUtils.barcodePosPay("1", "1", barcodePosPayModel);
             return GsonUtils.toJson(result);
         } catch (Exception e) {
             return e.getMessage();
