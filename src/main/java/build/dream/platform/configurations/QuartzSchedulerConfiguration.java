ffab7668dc7f91502a331d18c2d91742edb8c3ab
liuyandong <554030404@qq.com>
2018-09-13 11:24:24 +0800

    微信相关接口开发！！

--- src/main/java/build/dream/platform/configurations/QuartzSchedulerConfiguration.java
+++ src/main/java/build/dream/platform/configurations/QuartzSchedulerConfiguration.java
@@ -1,12 +1,18 @@
 package build.dream.platform.configurations;
 
+import build.dream.platform.constants.Constants;
 import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.scheduling.quartz.AdaptableJobFactory;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 /**
  * Created by liuyandong on 2017/09/20.
@@ -14,10 +20,21 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 @Configuration
 public class QuartzSchedulerConfiguration {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
     @Bean
     public SchedulerFactoryBean schedulerFactoryBean(JobFactory jobFactory) {
         SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
         schedulerFactoryBean.setJobFactory(jobFactory);
+        Resource configLocation = new ClassPathResource(Constants.QUARTZ_PROPERTIES);
+        schedulerFactoryBean.setConfigLocation(configLocation);
+        schedulerFactoryBean.setDataSource(dataSource);
+        schedulerFactoryBean.setTransactionManager(platformTransactionManager);
+        schedulerFactoryBean.setSchedulerName(Constants.SCHEDULER_NAME);
+        schedulerFactoryBean.setApplicationContextSchedulerContextKey(Constants.APPLICATION_CONTEXT);
         return schedulerFactoryBean;
     }
 
--- src/main/java/build/dream/platform/jobs/JobScheduler.java
+++ src/main/java/build/dream/platform/jobs/JobScheduler.java
@@ -14,36 +14,81 @@ public class JobScheduler {
     SchedulerFactoryBean schedulerFactoryBean;
 
     public void scheduler() {
-        Scheduler scheduler = schedulerFactoryBean.getScheduler();
         try {
             // 启动同步门店信息定时任务
-            String synchronizeBranchInfoJobCronExpression = ConfigurationUtils.getConfiguration(Constants.SYNCHRONIZE_BRANCH_INFO_JOB_CRON_EXPRESSION);
-            if (StringUtils.isNotBlank(synchronizeBranchInfoJobCronExpression)) {
-                JobDetail synchronizeBranchInfoJobDetail = JobBuilder.newJob(SynchronizeBranchInfoJob.class).withIdentity("synchronizeBranchInfoJob", "platformJobGroup").build();
-                CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(synchronizeBranchInfoJobCronExpression);
-                Trigger synchronizeBranchInfoJobCronTrigger = TriggerBuilder.newTrigger().withIdentity("synchronizeBranchInfoJobTrigger", "platformJobGroup").withSchedule(cronScheduleBuilder).build();
-                scheduler.scheduleJob(synchronizeBranchInfoJobDetail, synchronizeBranchInfoJobCronTrigger);
-            }
+            startSynchronizeBranchInfoJob();
 
             // 启用禁用门店产品定时任务
-            String disableBranchGoodsJobCronExpression = ConfigurationUtils.getConfiguration(Constants.DISABLE_BRANCH_GOODS_JOB_CRON_EXPRESSION);
-            if (StringUtils.isNotBlank(disableBranchGoodsJobCronExpression)) {
-                JobDetail disableBranchGoodsJobDetail = JobBuilder.newJob(DisableBranchGoodsJob.class).withIdentity("disableBranchGoodsJob", "platformJobGroup").build();
-                CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(disableBranchGoodsJobCronExpression);
-                Trigger disableBranchGoodsJobCronTrigger = TriggerBuilder.newTrigger().withIdentity("disableBranchGoodsJobTrigger", "platformJobGroup").withSchedule(cronScheduleBuilder).build();
-                scheduler.scheduleJob(disableBranchGoodsJobDetail, disableBranchGoodsJobCronTrigger);
-            }
+            startDisableBranchGoodsJob();
 
             // 启动刷新微信授权token定时任务
-            String refreshWeiXinAuthorizerTokenJobCronExpression = ConfigurationUtils.getConfiguration(Constants.REFRESH_WEI_XIN_AUTHORIZER_TOKEN_JOB_CRON_EXPRESSION);
-            if (StringUtils.isNotBlank(refreshWeiXinAuthorizerTokenJobCronExpression)) {
-                JobDetail refreshWeiXinAuthorizerTokenJobDetail = JobBuilder.newJob(RefreshWeiXinAuthorizerTokenJob.class).withIdentity("refreshWeiXinAuthorizerTokenJob", "platformJobGroup").build();
-                CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(refreshWeiXinAuthorizerTokenJobCronExpression);
-                Trigger refreshWeiXinAuthorizerTokenJobCronTrigger = TriggerBuilder.newTrigger().withIdentity("refreshWeiXinAuthorizerTokenJobTrigger", "platformJobGroup").withSchedule(cronScheduleBuilder).build();
-                scheduler.scheduleJob(refreshWeiXinAuthorizerTokenJobDetail, refreshWeiXinAuthorizerTokenJobCronTrigger);
-            }
+            startRefreshWeiXinAuthorizerTokenJob();
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void stopJob(JobKey jobKey, TriggerKey triggerKey) throws SchedulerException {
+        Scheduler scheduler = schedulerFactoryBean.getScheduler();
+        if (scheduler.checkExists(jobKey) && scheduler.checkExists(triggerKey)) {
+            scheduler.pauseTrigger(triggerKey);
+            scheduler.unscheduleJob(triggerKey);
+
+            scheduler.deleteJob(jobKey);
+        }
+    }
+
+    private void startSynchronizeBranchInfoJob() throws SchedulerException {
+        JobKey jobKey = JobKey.jobKey("synchronizeBranchInfoJob", "platformJobGroup");
+        TriggerKey triggerKey = TriggerKey.triggerKey("synchronizeBranchInfoJobTrigger", "platformJobGroup");
+        String synchronizeBranchInfoJobCronExpression = ConfigurationUtils.getConfiguration(Constants.SYNCHRONIZE_BRANCH_INFO_JOB_CRON_EXPRESSION);
+        if (StringUtils.isNotBlank(synchronizeBranchInfoJobCronExpression)) {
+            stopJob(jobKey, triggerKey);
+
+            JobDetail synchronizeBranchInfoJobDetail = JobBuilder.newJob(SynchronizeBranchInfoJob.class).withIdentity(jobKey).build();
+            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(synchronizeBranchInfoJobCronExpression);
+            Trigger synchronizeBranchInfoJobCronTrigger = TriggerBuilder.newTrigger().withIdentity(triggerKey).withSchedule(cronScheduleBuilder).build();
+
+            Scheduler scheduler = schedulerFactoryBean.getScheduler();
+            scheduler.scheduleJob(synchronizeBranchInfoJobDetail, synchronizeBranchInfoJobCronTrigger);
+        } else {
+            stopJob(jobKey, triggerKey);
+        }
+    }
+
+    private void startDisableBranchGoodsJob() throws SchedulerException {
+        JobKey jobKey = JobKey.jobKey("disableBranchGoodsJob", "platformJobGroup");
+        TriggerKey triggerKey = TriggerKey.triggerKey("disableBranchGoodsJobTrigger", "platformJobGroup");
+        String disableBranchGoodsJobCronExpression = ConfigurationUtils.getConfiguration(Constants.DISABLE_BRANCH_GOODS_JOB_CRON_EXPRESSION);
+        if (StringUtils.isNotBlank(disableBranchGoodsJobCronExpression)) {
+            stopJob(jobKey, triggerKey);
+
+            JobDetail disableBranchGoodsJobDetail = JobBuilder.newJob(DisableBranchGoodsJob.class).withIdentity(jobKey).build();
+            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(disableBranchGoodsJobCronExpression);
+            Trigger disableBranchGoodsJobCronTrigger = TriggerBuilder.newTrigger().withIdentity(triggerKey).withSchedule(cronScheduleBuilder).build();
+
+            Scheduler scheduler = schedulerFactoryBean.getScheduler();
+            scheduler.scheduleJob(disableBranchGoodsJobDetail, disableBranchGoodsJobCronTrigger);
+        } else {
+            stopJob(jobKey, triggerKey);
+        }
+    }
+
+    public void startRefreshWeiXinAuthorizerTokenJob() throws SchedulerException {
+        JobKey jobKey = JobKey.jobKey("refreshWeiXinAuthorizerTokenJob", "platformJobGroup");
+        TriggerKey triggerKey = TriggerKey.triggerKey("refreshWeiXinAuthorizerTokenJobTrigger", "platformJobGroup");
+        String refreshWeiXinAuthorizerTokenJobCronExpression = ConfigurationUtils.getConfiguration(Constants.REFRESH_WEI_XIN_AUTHORIZER_TOKEN_JOB_CRON_EXPRESSION);
+        if (StringUtils.isNotBlank(refreshWeiXinAuthorizerTokenJobCronExpression)) {
+            stopJob(jobKey, triggerKey);
+
+            JobDetail refreshWeiXinAuthorizerTokenJobDetail = JobBuilder.newJob(RefreshWeiXinAuthorizerTokenJob.class).withIdentity(jobKey).build();
+            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(refreshWeiXinAuthorizerTokenJobCronExpression);
+            Trigger refreshWeiXinAuthorizerTokenJobCronTrigger = TriggerBuilder.newTrigger().withIdentity(triggerKey).withSchedule(cronScheduleBuilder).build();
+
+            Scheduler scheduler = schedulerFactoryBean.getScheduler();
+            scheduler.scheduleJob(refreshWeiXinAuthorizerTokenJobDetail, refreshWeiXinAuthorizerTokenJobCronTrigger);
+        } else {
+            stopJob(jobKey, triggerKey);
         }
     }
 }
