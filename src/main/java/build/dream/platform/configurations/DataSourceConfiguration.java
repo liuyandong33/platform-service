f0a441fe0f8eaab1cde96b29db10754930be7758
liuyandong <554030404@qq.com>
2018-10-15 16:15:21 +0800

    微信相关接口开发！！

--- src/main/java/build/dream/platform/configurations/DataSourceConfiguration.java
+++ src/main/java/build/dream/platform/configurations/DataSourceConfiguration.java
@@ -3,39 +3,14 @@ package build.dream.platform.configurations;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class DataSourceConfiguration {
-    @Bean(name = "primaryDataSource")
-    @ConfigurationProperties(prefix = "primary.datasource")
-    public DataSource primaryDataSource() {
+    @ConfigurationProperties(prefix = "datasource")
+    public DataSource dataSource() {
         return DataSourceBuilder.create().type(DruidDataSource.class).build();
-    }
-
-    @Bean(name = "secondaryDataSource")
-    @ConfigurationProperties(prefix = "secondary.datasource")
-    public DataSource secondaryDataSource() {
-        return DataSourceBuilder.create().type(DruidDataSource.class).build();
-    }
-
-    @Bean
-    @Primary
-    public RoutingDataSource routingDataSource() {
-        RoutingDataSource routingDataSource = new RoutingDataSource();
-        DataSource primaryDataSource = primaryDataSource();
-        DataSource secondaryDataSource = secondaryDataSource();
-        Map<Object, Object> targetDataSources = new HashMap<Object, Object>();
-        targetDataSources.put("primaryDataSource", primaryDataSource);
-        targetDataSources.put("secondaryDataSource", secondaryDataSource);
-        routingDataSource.setTargetDataSources(targetDataSources);
-        routingDataSource.setDefaultTargetDataSource(primaryDataSource);
-        return routingDataSource;
     }
 }
--- src/main/java/build/dream/platform/configurations/WebMvcConfiguration.java
+++ src/main/java/build/dream/platform/configurations/WebMvcConfiguration.java
@@ -13,6 +13,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(accessLogHandlerInterceptor).addPathPatterns("/**");
+//        registry.addInterceptor(accessLogHandlerInterceptor).addPathPatterns("/**");
     }
 }
