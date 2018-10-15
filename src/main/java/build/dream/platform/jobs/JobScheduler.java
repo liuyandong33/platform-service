49795ae6a930b6ab7cac2cd741c8370e1cb150b4
liuyandong <554030404@qq.com>
2018-10-15 10:42:28 +0800

    微信相关接口开发！！

--- src/main/java/build/dream/platform/jobs/JobScheduler.java
+++ src/main/java/build/dream/platform/jobs/JobScheduler.java
@@ -1,18 +1,18 @@
 package build.dream.platform.jobs;
 
 import build.dream.common.utils.ConfigurationUtils;
+import build.dream.common.utils.JobUtils;
 import build.dream.platform.constants.Constants;
 import org.apache.commons.lang.StringUtils;
-import org.quartz.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.quartz.JobKey;
+import org.quartz.SchedulerException;
+import org.quartz.TriggerKey;
 import org.springframework.stereotype.Component;
 
-@Component
-public class JobScheduler {
-    @Autowired
-    SchedulerFactoryBean schedulerFactoryBean;
+import java.io.Serializable;
 
+@Component
+public class JobScheduler implements Serializable {
     public void scheduler() {
         try {
             // 启动同步门店信息定时任务
@@ -29,12 +29,10 @@ public class JobScheduler {
     }
 
     private void stopJob(JobKey jobKey, TriggerKey triggerKey) throws SchedulerException {
-        Scheduler scheduler = schedulerFactoryBean.getScheduler();
-        if (scheduler.checkExists(jobKey) && scheduler.checkExists(triggerKey)) {
-            scheduler.pauseTrigger(triggerKey);
-            scheduler.unscheduleJob(triggerKey);
-
-            scheduler.deleteJob(jobKey);
+        if (JobUtils.checkExists(jobKey) && JobUtils.checkExists(triggerKey)) {
+            JobUtils.pauseTrigger(triggerKey);
+            JobUtils.unscheduleJob(triggerKey);
+            JobUtils.deleteJob(jobKey);
         }
     }
 
@@ -44,13 +42,7 @@ public class JobScheduler {
         String synchronizeBranchInfoJobCronExpression = ConfigurationUtils.getConfiguration(Constants.SYNCHRONIZE_BRANCH_INFO_JOB_CRON_EXPRESSION);
         if (StringUtils.isNotBlank(synchronizeBranchInfoJobCronExpression)) {
             stopJob(jobKey, triggerKey);
-
-            JobDetail synchronizeBranchInfoJobDetail = JobBuilder.newJob(SynchronizeBranchInfoJob.class).withIdentity(jobKey).build();
-            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(synchronizeBranchInfoJobCronExpression);
-            Trigger synchronizeBranchInfoJobCronTrigger = TriggerBuilder.newTrigger().withIdentity(triggerKey).withSchedule(cronScheduleBuilder).build();
-
-            Scheduler scheduler = schedulerFactoryBean.getScheduler();
-            scheduler.scheduleJob(synchronizeBranchInfoJobDetail, synchronizeBranchInfoJobCronTrigger);
+            JobUtils.scheduleCronJob(SynchronizeBranchInfoJob.class, jobKey, triggerKey, synchronizeBranchInfoJobCronExpression, null);
         } else {
             stopJob(jobKey, triggerKey);
         }
@@ -62,13 +54,7 @@ public class JobScheduler {
         String disableBranchGoodsJobCronExpression = ConfigurationUtils.getConfiguration(Constants.DISABLE_BRANCH_GOODS_JOB_CRON_EXPRESSION);
         if (StringUtils.isNotBlank(disableBranchGoodsJobCronExpression)) {
             stopJob(jobKey, triggerKey);
-
-            JobDetail disableBranchGoodsJobDetail = JobBuilder.newJob(DisableBranchGoodsJob.class).withIdentity(jobKey).build();
-            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(disableBranchGoodsJobCronExpression);
-            Trigger disableBranchGoodsJobCronTrigger = TriggerBuilder.newTrigger().withIdentity(triggerKey).withSchedule(cronScheduleBuilder).build();
-
-            Scheduler scheduler = schedulerFactoryBean.getScheduler();
-            scheduler.scheduleJob(disableBranchGoodsJobDetail, disableBranchGoodsJobCronTrigger);
+            JobUtils.scheduleCronJob(DisableBranchGoodsJob.class, jobKey, triggerKey, disableBranchGoodsJobCronExpression, null);
         } else {
             stopJob(jobKey, triggerKey);
         }
@@ -80,13 +66,7 @@ public class JobScheduler {
         String refreshWeiXinAuthorizerTokenJobCronExpression = ConfigurationUtils.getConfiguration(Constants.REFRESH_WEI_XIN_AUTHORIZER_TOKEN_JOB_CRON_EXPRESSION);
         if (StringUtils.isNotBlank(refreshWeiXinAuthorizerTokenJobCronExpression)) {
             stopJob(jobKey, triggerKey);
-
-            JobDetail refreshWeiXinAuthorizerTokenJobDetail = JobBuilder.newJob(RefreshWeiXinAuthorizerTokenJob.class).withIdentity(jobKey).build();
-            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(refreshWeiXinAuthorizerTokenJobCronExpression);
-            Trigger refreshWeiXinAuthorizerTokenJobCronTrigger = TriggerBuilder.newTrigger().withIdentity(triggerKey).withSchedule(cronScheduleBuilder).build();
-
-            Scheduler scheduler = schedulerFactoryBean.getScheduler();
-            scheduler.scheduleJob(refreshWeiXinAuthorizerTokenJobDetail, refreshWeiXinAuthorizerTokenJobCronTrigger);
+            JobUtils.scheduleCronJob(RefreshWeiXinAuthorizerTokenJob.class, jobKey, triggerKey, refreshWeiXinAuthorizerTokenJobCronExpression, null);
         } else {
             stopJob(jobKey, triggerKey);
         }
