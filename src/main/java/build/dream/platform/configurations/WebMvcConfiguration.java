86c0184f8a16f217d893980bbd83347b7069d283
liuyandong <554030404@qq.com>
2019-07-20 16:54:08 +0800

    构建脚本修改！！

--- src/main/java/build/dream/platform/configurations/WebMvcConfiguration.java
+++ src/main/java/build/dream/platform/configurations/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package build.dream.platform.configurations;
 
 import build.dream.platform.interceptors.AccessLogHandlerInterceptor;
+import build.dream.platform.interceptors.ThreadNameHandlerInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,9 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfiguration implements WebMvcConfigurer {
     @Autowired
     private AccessLogHandlerInterceptor accessLogHandlerInterceptor;
+    @Autowired
+    private ThreadNameHandlerInterceptor threadNameHandlerInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(accessLogHandlerInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(threadNameHandlerInterceptor).addPathPatterns("/**");
     }
 }
---
+++ src/main/java/build/dream/platform/interceptors/ThreadNameHandlerInterceptor.java
@@ -0,0 +1,25 @@
+package build.dream.platform.interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class ThreadNameHandlerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        String className = handlerMethod.getBeanType().getName();
+        String methodName = handlerMethod.getMethod().getName();
+        Thread thread = Thread.currentThread();
+        thread.setName(className + "." + methodName + "@" + thread.getId());
+        return true;
+    }
+}
