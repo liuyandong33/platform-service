448ffb83f131155e3127c3c861f6b6a6661ede2d
liuyandong <554030404@qq.com>
2020-03-04 03:33:11 +0800

    错误修改！！

--- src/main/java/build/dream/platform/Application.java
+++ src/main/java/build/dream/platform/Application.java
@@ -3,12 +3,13 @@ package build.dream.platform;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @ServletComponentScan
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan(basePackages = {"build.dream.common.mappers", "build.dream.platform.mappers"})
